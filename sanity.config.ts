f428008a813cbfd822fc7e6894babe79df97f5f7
saririvaii <sarii.rivaii@gmail.com>
2025-09-20 20:19:06 +1000

    update required packages

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
-import { codeInput } from '@sanity/code-input'
 import { schemaTypes } from './sanity/schemas'
 
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   basePath: '/studio',
-  plugins: [structureTool(), visionTool(), codeInput()],
+  plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
